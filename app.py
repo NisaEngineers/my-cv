e5034b8cbe3d3963ef2fd3c5a796d124881d6755
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2023-06-15 18:33:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from pathlib import Path
-
+import os
 import streamlit as st
 from PIL import Image
 
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
-profile_pic = current_dir / "assets" / "my_.jpg"
+css_file = "styles/main.css"
+resume_file = "assets/CV.pdf"
+profile_pic = "assets/my_.jpg"
 
 
 # --- GENERAL SETTINGS ---
