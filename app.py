879e8fa323af7e276e53232f44f6d4490c12bcf0
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2023-08-08 22:14:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = "main.css"
 resume_file = "assets/CV.pdf"
-profile_pic = "my_.jpg"
+profile_pic = "new.jpg"
 
 
 # --- GENERAL SETTINGS ---
